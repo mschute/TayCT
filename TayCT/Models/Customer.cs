13f842618412fb59c6583d7b72177024e1c3edba
mschute <46330682+mschute@users.noreply.github.com>
2024-02-28 21:52:18 +0000

    Created Customer.cs file for the model. Based on the Code-first approach using ICollection to connection a one-to-many relationship.

---
+++ TayCT/Models/Customer.cs
@@ -0,0 +1,12 @@
+namespace TCT.Models;
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateOnly? Dob { get; set; }
+    public string? Nationality { get; set; }
+    
+    public ICollection<Booking> Bookings { get; set; }
+}
